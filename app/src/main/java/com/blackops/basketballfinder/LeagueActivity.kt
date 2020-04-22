e44bb1b671b633251d6c10cf1f7f5ad938782bcc
Seleman Hemed <shemed.blackops@gmail.com>
2020-04-22 14:57:46 +0300

    Adding functionality to getStarted button

--- app/src/main/java/com/blackops/basketballfinder/LeagueActivity.kt
+++ app/src/main/java/com/blackops/basketballfinder/LeagueActivity.kt
@@ -1,12 +1,13 @@
 package com.blackops.basketballfinder
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_league)
     }
 }
--- app/src/main/java/com/blackops/basketballfinder/MainActivity.kt
+++ app/src/main/java/com/blackops/basketballfinder/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.blackops.basketballfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        get_started_button.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
