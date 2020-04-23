9a7f10dd5af981c992a0af49ee95c5ea9c5bc7b0
Seleman Hemed <shemed.blackops@gmail.com>
2020-04-23 21:01:56 +0300

    Adding skill layout and functionality to next button

---
+++ app/src/main/java/com/blackops/basketballfinder/BaseActivity.kt
@@ -0,0 +1,43 @@
+package com.blackops.basketballfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LyfeCycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "${javaClass.simpleName} OnCreate")
+    }
+
+    override fun onStart() {
+        Log.d(TAG, "${javaClass.simpleName} onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "${javaClass.simpleName} onResume")
+        super.onResume()
+    }
+    override fun onPause() {
+        Log.d(TAG, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.d(TAG, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "${javaClass.simpleName} OnCreate")
+        super.onDestroy()
+    }
+
+}
--- app/src/main/java/com/blackops/basketballfinder/LeagueActivity.kt
+++ app/src/main/java/com/blackops/basketballfinder/LeagueActivity.kt
@@ -1,13 +1,23 @@
 package com.blackops.basketballfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun leagueNextClicked(view : View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
+    }
+
+
 }
