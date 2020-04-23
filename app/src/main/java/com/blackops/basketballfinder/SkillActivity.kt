d7e668d071741debc6fd35581bf240af16c4b63a
Seleman Hemed <shemed.blackops@gmail.com>
2020-04-23 21:52:30 +0300

    Passing league value to skill activity

--- app/src/main/java/com/blackops/basketballfinder/SkillActivity.kt
+++ app/src/main/java/com/blackops/basketballfinder/SkillActivity.kt
@@ -2,11 +2,15 @@ package com.blackops.basketballfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+    var league = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        Toast.makeText(this, league, Toast.LENGTH_SHORT).show()
     }
 }
